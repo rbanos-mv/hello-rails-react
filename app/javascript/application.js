b4f7c11b49ecadded651c4cd914c3e60f36c43ad
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-09-14 08:56:23 -0500

    Create App component and setup provider

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import store from './redux/configureStore';
 
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+// store.dispatch(getGreeting());
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root'),
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 );
